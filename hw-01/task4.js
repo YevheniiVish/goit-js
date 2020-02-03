9606dee3fc1c9e7e36b24ae2c4c68a3360332162
YevheniiVish <55807961+YevheniiVish@users.noreply.github.com>
2020-02-03 07:32:19 +0200

    Add files via upload

---
+++ hw-01/task4.js
@@ -0,0 +1,25 @@
+"use strict";
+const credits = 23580;
+const pricePerDroid = 3000;
+const promptlabel = `Ведите количество дроидов которые вы желаете купить`;
+let userBuy = prompt(promptlabel);
+let quantityPerDroid = Number(userBuy); /*к-во дроидов*/
+let totalPrice; /* сумма заказа*/
+let userBalance; /*баланс (остаток) средств на счете пользователя*/
+let message;
+
+if (userBuy === null) {
+  message = "Отменено пользователем!";
+  alert(message);
+} else {
+  totalPrice = quantityPerDroid * pricePerDroid;
+
+  if (totalPrice > credits) {
+    console.log("Недостаточно средств на счете!");
+  } else {
+    userBalance = credits - totalPrice;
+    console.log(
+      `Вы купили ${quantityPerDroid} дроидов, на счету осталось ${userBalance} кредитов`
+    );
+  }
+}
