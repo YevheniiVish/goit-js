28ad5505d5da80cc5aa48381f837e3e2daf1e9fa
YevheniiVish <55807961+YevheniiVish@users.noreply.github.com>
2020-02-11 06:24:41 +0200

    Add files via upload

--- hw-01/task5.js
+++ hw-01/task5.js
@@ -1,56 +1,56 @@
-// "use strict";
-// const countryCreditChina = 100;
-// const countryCreditChile = 250;
-// const countryCreditAustralia = 170;
-// const countryCreditIndia = 80;
-// const countryCreditJamaica = 120;
+"use strict";
+const countryCreditChina = 100;
+const countryCreditChile = 250;
+const countryCreditAustralia = 170;
+const countryCreditIndia = 80;
+const countryCreditJamaica = 120;
 
-// let result = prompt("Ведите вашу страну", "");
+let result = prompt("Ведите вашу страну", "");
 
-// if (result === null) {
-//   alert("cancel");
-// } else {
-//   const country = result.toLowerCase();
-//   switch (country) {
-//     case "китай": {
-//       countryCreditChina;
-//       alert(
-//         `Доставка в ${country} будет стоить ${countryCreditChina} кредитов`
-//       );
-//       break;
-//     }
-//     case "чили": {
-//       countryCreditChile;
-//       alert(
-//         `Доставка в ${country} будет стоить ${countryCreditChile} кредитов`
-//       );
-//       break;
-//     }
-//     case "австралия": {
-//       countryCreditAustralia;
-//       alert(
-//         `Доставка в ${country} будет стоить ${countryCreditAustralia} кредитов`
-//       );
-//       break;
-//     }
-//     case "индия": {
-//       countryCreditIndia;
-//       alert(
-//         `Доставка в ${country} будет стоить ${countryCreditIndia} кредитов`
-//       );
-//       break;
-//     }
-//     case "ямайка": {
-//       countryCreditJamaica;
-//       alert(
-//         `Доставка в ${country} будет стоить ${countryCreditJamaica} кредитов`
-//       );
-//       break;
-//     }
+if (result === null) {
+  alert("cancel");
+} else {
+  const country = result.toLowerCase();
+  switch (country) {
+    case "китай": {
+      countryCreditChina;
+      alert(
+        `Доставка в ${country} будет стоить ${countryCreditChina} кредитов`
+      );
+      break;
+    }
+    case "чили": {
+      countryCreditChile;
+      alert(
+        `Доставка в ${country} будет стоить ${countryCreditChile} кредитов`
+      );
+      break;
+    }
+    case "австралия": {
+      countryCreditAustralia;
+      alert(
+        `Доставка в ${country} будет стоить ${countryCreditAustralia} кредитов`
+      );
+      break;
+    }
+    case "индия": {
+      countryCreditIndia;
+      alert(
+        `Доставка в ${country} будет стоить ${countryCreditIndia} кредитов`
+      );
+      break;
+    }
+    case "ямайка": {
+      countryCreditJamaica;
+      alert(
+        `Доставка в ${country} будет стоить ${countryCreditJamaica} кредитов`
+      );
+      break;
+    }
 
-//     default: {
-//       alert("В вашей стране доставка не доступна");
-//       break;
-//     }
-//   }
-// }
+    default: {
+      alert("В вашей стране доставка не доступна");
+      break;
+    }
+  }
+}
