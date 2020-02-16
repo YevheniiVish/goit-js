6cb42bbf96739d82c77f507d7e809e477587c7c9
YevheniiVish <55807961+YevheniiVish@users.noreply.github.com>
2020-02-16 15:09:17 +0200

    Add files via upload

--- hw-03/task1.js
+++ hw-03/task1.js
@@ -1,33 +1,34 @@
-// Напиши скрипт, который, для объекта user, последовательно:
+// // Напиши скрипт, который, для объекта user, последовательно:
 
-// добавляет поле mood со значением 'happy'
-// заменяет значение hobby на 'skydiving'
-// заменяет значение premium на false
-// выводит содержимое объекта user в формате ключ:значение используя Object.keys() и for...of
+// // добавляет поле mood со значением 'happy'
+// // заменяет значение hobby на 'skydiving'
+// // заменяет значение premium на false
+// // выводит содержимое объекта user в формате ключ:значение используя Object.keys() и for...of
 
 
-const user = {
-  name: 'Mango',
-  age: 20,
-  hobby: 'html',
-  premium: true,
-};
+// const user = {
+//   name: 'Mango',
+//   age: 20,
+//   hobby: 'html',
+//   premium: true,
+// };
 
-console.log('user: ', user);
+// console.log('user: ', user);
 
-user.mood = 'happy';
-user.hobby = 'skydiving';
-user.premium = false;
+// user.mood = 'happy';
+// user.hobby = 'skydiving';
+// user.premium = false;
 
    
- const keys = Object.keys(user);
+//  const keys = Object.keys(user);
  
-  for (const key of keys) {
-    console.log(key);
+//   for (const key of keys) {
+//     console.log('key: ', key);
+//     console.log('user[key]: ', user[key]);
     
-  }
+//     }
 
 
-console.log('userModified:', user);
+// console.log('userModified:', user);
 
 
---
+++ hw-03/task2.js
@@ -0,0 +1,14 @@
+// Напиши функцию countProps(obj), считающую кол-во свойств в объекте. Функция возвращает число - количество свойств.
+
+const countProps = function(obj) {
+  // твой код
+  for (i = 0; i <= countProps.length; i += 1) {
+    return Object.entries(obj).length;
+  }
+};
+
+console.log(countProps({})); // 0
+
+console.log(countProps({ name: "Mango", age: 2 })); // 2
+
+console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 })); // 3s
